e513ca2f27e94dfaad0452e98d2b66bc652a6733
thnhan1203 <thanhnhan@example.com>
2026-07-04 11:24:28 +0700

    Update Google Apps Script endpoint

--- src/js/config.js
+++ src/js/config.js
@@ -3,5 +3,5 @@
 // following docs/google-sheets-setup.md.
 
 window.SOHUB_CONFIG = window.SOHUB_CONFIG || {
-  sheetsEndpoint: 'https://script.google.com/macros/s/AKfycbw2LW_PClP958nkIGPWBIDIbV15J5pRbbx9msL5kH-H-mdUj8UX00x58k3H_ThHT5bHZA/exec'
+  sheetsEndpoint: 'https://script.google.com/macros/s/AKfycbz6pvR-eXAS9DsiMJ1AzdSWYrzQg_1cL_6LuhQwCjk8OlJMUQLm50mg1fqnGKHXQ41LFA/exec'
 };
